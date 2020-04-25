36ed603b9e311268516943aa77bc16401ce7130d
vietpd <viet3695@gmail.com>
2020-04-25 11:27:33 +0700

    update script

--- docker/hbase/run.sh
+++ docker/hbase/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "HBASE_IP: $(hostname)"
+
 docker run --rm -d -h "$(hostname)" \
   -p 2181:2181 -p 60000:60000 -p 60010:60010 -p 60020:60020 -p 60030:60030 \
   --name hbase debian-hbase
