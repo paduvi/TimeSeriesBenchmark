ef86c822a1acc6bb509ce220a8553e93cc3b70c4
My Tien Bui <zennguyen.hcm@gmail.com>
2020-04-28 15:53:41 +0700

    add tsdb constructor

--- src/main/java/com/techpago/dao/impl/OpentsdbUserNotifyDao.java
+++ src/main/java/com/techpago/dao/impl/OpentsdbUserNotifyDao.java
@@ -1,5 +1,7 @@
 package com.techpago.dao.impl;
 
+import com.sun.xml.internal.ws.api.config.management.policy.ManagementAssertion;
+import com.techpago.config.Settings;
 import com.techpago.dao.IUserNotifyDao;
 import com.techpago.model.UserNotify;
 import net.opentsdb.core.TSDB;
@@ -8,6 +10,8 @@ import net.opentsdb.tools.OpenTSDBMain;
 import net.opentsdb.utils.Config;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 @Component
@@ -15,6 +19,10 @@ public class OpentsdbUserNotifyDao implements IUserNotifyDao {
     public OpentsdbUserNotifyDao(){
         TSDB tsdbWrite = createTsdb();
         TSDB tsdbRead = createTsdb();
+    }
+    @PostConstruct
+    void init() throws IOException{
+
     }
     @Override
     public void insert(UserNotify userNotify) throws Exception {
@@ -36,8 +44,25 @@ public class OpentsdbUserNotifyDao implements IUserNotifyDao {
         return null;
     }
 
+    @Override
+    public void flushDB() throws Exception {
+
+    }
+
     private TSDB createTsdb(){
+        Settings setting = Settings.getInstance();
         Config config = new Config();
+
+        config.overrideConfig("tsd.network.port", "4242"); //The TCP port to use for accepting connections
+        config.overrideConfig("tsd.http.staticroot", "/usr/share/opentsdb/static"); //Location of a directory where static files
+        config.overrideConfig("tsd.http.cachedir", "/tmp/opentsdb"); //The full path to a location where temporary files can be written
+        config.overrideConfig("tsd.core.auto_create_metrics", "true"); //Create new metrics or throw exception if it not exist.
+        config.overrideConfig("tsd.core.meta.enable_tsuid_incrementing", "true");
+        config.overrideConfig("tsd.storage.hbase.data_table", "tsdb");//Name of the HBase table where data points are stored
+        config.overrideConfig("tsd.storage.hbase.uid_table", "tsdb-uid");//Name of the HBase table where UID information is stored
+        config.overrideConfig("tsd.storage.hbase.zk_quorum",  String.join(",", setting.HBASE_IP)); //List of Zookeeper hosts that manage the HBase cluster
+        config.overrideConfig("tsd.storage.fix_duplicates", "true");
+
         TSDB tsdb = new TSDB(config);
         return  tsdb;
     }
